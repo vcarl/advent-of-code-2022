937c355953487cf3e86459c566c9054ae5549bdc
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-02 00:31:57 -0600

    Day 2 part 2

--- 2/code.js
+++ 2/code.js
@@ -1,21 +1,32 @@
 import { readFile } from "fs/promises";
-
+/*
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+*/
+// A for Rock, B for Paper, and C for Scissors
+// X for lose, Y for draw, Z for win
+// X for Rock, Y for Paper, and Z for Scissors
 const CHOICES = {
   X: 1,
   Y: 2,
   Z: 3,
 };
-// A for Rock, B for Paper, and C for Scissors
-// X for Rock, Y for Paper, and Z for Scissors
-const WINS = {
-  A: "Z",
-  B: "X",
-  C: "Y",
+const OUTCOME = {
+  X: 0,
+  Y: 3,
+  Z: 6,
 };
-const SAME = {
-  A: "X",
-  B: "Y",
-  C: "Z",
+const RESPONSES = {
+  "A X": "Z",
+  "A Y": "X",
+  "A Z": "Y",
+  "B X": "X",
+  "B Y": "Y",
+  "B Z": "Z",
+  "C X": "Y",
+  "C Y": "Z",
+  "C Z": "X",
 };
 
 const score = (await readFile("./2/input"))
@@ -23,12 +34,9 @@ const score = (await readFile("./2/input"))
   .trim()
   .split("\n")
   .reduce((sum, pair) => {
-    const [opponent, response] = pair.split(" ");
-    sum =
-      sum +
-      CHOICES[response] +
-      (SAME[opponent] === response ? 3 : response === WINS[opponent] ? 0 : 6);
-    console.log(opponent, response, sum);
+    const [_, outcome] = pair.split(" ");
+    sum = sum + CHOICES[RESPONSES[pair]] + OUTCOME[outcome];
+    console.log(pair, outcome, sum);
     return sum;
   }, 0);
 

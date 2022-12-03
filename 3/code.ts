2e980dd53bf8fc3d8857d78f9810b75b6fa7a794
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-02 23:34:00 -0600

    Day 3 part 2

--- 3/code.ts
+++ 3/code.ts
@@ -1,4 +1,6 @@
 import { readFile } from "fs/promises";
+import { chunk } from "lodash-es";
+
 const sum = (arr: number[]) => arr.reduce((sum, val) => sum + val, 0);
 const getPriority = (char: string) => {
   const codepoint = char.codePointAt(0) || -1;
@@ -6,18 +8,19 @@ const getPriority = (char: string) => {
   return codepoint > 95 ? codepoint - 96 : codepoint - 38;
 };
 
+const findBadge = (bags: string[]) => {
+  const [one, ...rest] = bags;
+  return one.split("").find((x) => rest.every((y) => y.includes(x))) || "";
+};
+
 const priority = sum(
-  (await readFile("./3/input"))
-    .toString("utf8")
-    .trim()
-    .split("\n")
-    .map((bag) => {
-      const one = bag.slice(0, bag.length / 2);
-      const two = bag.slice(bag.length / 2);
-      const match = one.split("").find((x) => two.includes(x)) || "";
-      const priority = getPriority(match);
-      return priority;
-    }),
+  chunk(
+    (await readFile("./3/input")).toString("utf8").trim().split("\n"),
+    3,
+  ).map((bags) => {
+    const priority = getPriority(findBadge(bags));
+    return priority;
+  }),
 );
 
 console.log(priority);

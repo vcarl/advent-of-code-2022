9a122647715a1d313db3169c98f5c89a3ce0582e
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-04 00:32:11 -0600

    Day 4 rough

--- 4/code.test.ts
+++ 4/code.test.ts
@@ -1,15 +1,21 @@
 import { describe, expect, it } from "vitest";
 import { day4part1, day4part2 } from "./code";
 
-const testInput = ``;
+const testInput = `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+`;
 
 describe("part 1", () => {
   it("works on test input", () => {
-    expect(day4part1(testInput)).toBe(NaN);
+    expect(day4part1(testInput)).toBe(2);
   });
 });
 describe("part 2", () => {
   it("works on test input", async () => {
-    expect(day4part2(testInput)).toBe(NaN);
+    expect(day4part2(testInput)).toBe(4);
   });
 });
--- 4/code.ts
+++ 4/code.ts
@@ -2,9 +2,49 @@ import { readFile } from "fs/promises";
 
 const input = (await readFile("./4/input")).toString("utf8").trim();
 
-export const day4part1 = (input: string) => {};
+const isContained = (a: string, b: string) => {
+  const [a1, a2] = a.split("-");
+  const [b1, b2] = b.split("-");
+  return (
+    (Number(a1) <= Number(b1) && Number(a2) >= Number(b2)) ||
+    (Number(b1) <= Number(a1) && Number(b2) >= Number(a2))
+  );
+};
 
-export const day4part2 = (input: string) => {};
+const isOverlapping = (a: string, b: string) => {
+  const [a1, a2] = a.split("-");
+  const [b1, b2] = b.split("-");
+  return (
+    (Number(a1) >= Number(b1) && Number(a1) <= Number(b2)) ||
+    (Number(a2) >= Number(b1) && Number(a2) <= Number(b2)) ||
+    (Number(b1) >= Number(a1) && Number(b1) <= Number(a2)) ||
+    (Number(b2) >= Number(a1) && Number(b2) <= Number(a2))
+  );
+};
+
+export const day4part1 = (input: string) => {
+  return input
+    .trim()
+    .split("\n")
+    .map((x) => {
+      const [one, two] = x.split(",");
+      const contained = isContained(one, two);
+      return contained;
+    })
+    .reduce((sum, val) => (val ? sum + 1 : sum), 0);
+};
+
+export const day4part2 = (input: string) => {
+  return input
+    .trim()
+    .split("\n")
+    .map((x) => {
+      const [one, two] = x.split(",");
+      const contained = isOverlapping(one, two);
+      return contained;
+    })
+    .reduce((sum, val) => (val ? sum + 1 : sum), 0);
+};
 
 console.log(day4part1(input));
 console.log(day4part2(input));

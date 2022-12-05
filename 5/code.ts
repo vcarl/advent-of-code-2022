d0e73ef9a91bb0bf11880ba3820e2acd0643a330
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-05 12:04:15 -0600

    Cleaned up day 5

--- 5/code.ts
+++ 5/code.ts
@@ -14,7 +14,7 @@ const parseContainers = (containers: string) => {
     (arr, val) => {
       val.split("").forEach((box, idx) => {
         if (box === "_") return;
-        arr[idx] ? arr[idx].push(box) : [box];
+        (arr[idx] ??= []).push(box);
       });
       return arr;
     },
@@ -23,53 +23,46 @@ const parseContainers = (containers: string) => {
   return stacks;
 };
 const instructionsRegex = /move (\d+) from (\d+) to (\d+)/;
-const parseInstructions = (instructions: string) => {
-  return instructions
+const parseInstructions = (instructions: string) =>
+  instructions
     .trim()
     .split("\n")
     .map((x) => {
-      const match = x.match(instructionsRegex)?.map(Number).slice(1);
-      return {
-        count: match![0],
-        from: match![1] - 1,
-        to: match![2] - 1,
-      };
+      const [count = 0, from = 0, to = 0] =
+        x.match(instructionsRegex)?.map(Number).slice(1) || [];
+      return { count, from: from - 1, to: to - 1 };
     });
+
+type Instruction = { from: number; to: number; count: number };
+type Stacks = string[][];
+const moveOne = (stacks: Stacks, { from, to, count }: Instruction) => {
+  for (let i = 0; i < count; i++) {
+    const val = stacks[from].pop()!;
+    stacks[to].push(val || "👀");
+  }
+};
+const moveMany = (stacks: Stacks, { from, to, count }: Instruction) => {
+  const val = stacks[from].slice(-count)!;
+  stacks[from] = stacks[from].slice(0, -count);
+  stacks[to] = stacks[to].concat(val);
 };
 
-const execute = (
-  stacks: string[][],
-  inst: { from: number; to: number; count: number }[],
+const takeLast = <T>(arr: T[]) => arr[arr.length - 1];
+const join = (accum: string, val: string) => accum.concat(val);
+const moveCrates = (
+  input: string,
+  strategy: (stacks: Stacks, i: Instruction) => void,
 ) => {
-  inst.forEach((instruction) => {
-    const val = stacks[instruction.from].slice(-instruction.count)!;
-    stacks[instruction.from] = stacks[instruction.from].slice(
-      0,
-      -instruction.count,
-    );
-    stacks[instruction.to] = stacks[instruction.to].concat(val || "👀");
-  });
-  const out = stacks.reduce((accum, val) => {
-    accum = accum.concat(val[val.length - 1]);
-    return accum;
-  }, "");
-  return out;
+  const [containerInput, instructionsInput] = input.split("\n\n");
+  const stacks = parseContainers(containerInput);
+  const instructions = parseInstructions(instructionsInput);
+  instructions.forEach((i) => strategy(stacks, i));
+  return stacks.map(takeLast).reduce(join);
 };
 
-export const day4part1 = (input: string) => {
-  const [containers, instructions] = input.split("\n\n");
-  const stacks = parseContainers(containers);
-  console.log(containers);
-  console.log(stacks.map((x, i) => `${i + 1} ` + x.join("")).join("\n"));
-  console.log("");
-  return execute(stacks, parseInstructions(instructions));
-};
+export const day4part1 = (input: string) => moveCrates(input, moveOne);
 
-// export const day4part2 = (input: string) =>
-// input
-//   .trim()
-//   .map(parseContainers)
-//   .reduce((sum, val) => (val ? sum + 1 : sum), 0);
+export const day4part2 = (input: string) => moveCrates(input, moveMany);
 
 console.log(day4part1(input));
-// console.log(day4part2(input));
+console.log(day4part2(input));

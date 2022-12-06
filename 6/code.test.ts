51e1aa0f0748bc56e25d76e4bbb9938d04dd9eba
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-06 11:38:26 -0600

    Day 6 part 2 rough

--- 6/code.test.ts
+++ 6/code.test.ts
@@ -3,15 +3,19 @@ import { day6part1, day4part2 } from "./code";
 
 describe("part 1", () => {
   it("works on test input", () => {
-    expect(day6part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(7);
-    expect(day6part1("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(5);
-    expect(day6part1("nppdvjthqldpwncqszvftbrmjlhg")).toBe(6);
-    expect(day6part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toBe(10);
-    expect(day6part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toBe(11);
+    // expect(day6part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(7);
+    // expect(day6part1("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(5);
+    // expect(day6part1("nppdvjthqldpwncqszvftbrmjlhg")).toBe(6);
+    // expect(day6part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toBe(10);
+    // expect(day6part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toBe(11);
   });
 });
 describe("part 2", () => {
   it("works on test input", async () => {
-    // expect(day4part2(testInput)).toBe(4);
+    expect(day6part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb")).toBe(19);
+    expect(day6part1("bvwbjplbgvbhsrlpgdmjqwftvncz")).toBe(23);
+    expect(day6part1("nppdvjthqldpwncqszvftbrmjlhg")).toBe(23);
+    expect(day6part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")).toBe(29);
+    expect(day6part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")).toBe(26);
   });
 });
--- 6/code.ts
+++ 6/code.ts
@@ -2,10 +2,17 @@ import { readFile } from "fs/promises";
 
 const input = (await readFile("./6/input")).toString("utf8");
 
+const hasMarker = (substr: string) => {
+  const x = new Set(substr.split(""));
+  if (x.size === 14) {
+    return true;
+  }
+};
+
 export const day6part1 = (input: string) => {
-  for (let i = 4; i < input.length; i++) {
-    const x = new Set(input.slice(i - 4, i).split(""));
-    if (x.size === 4) {
+  const out = [];
+  for (let i = 14; i < input.length; i++) {
+    if (hasMarker(input.slice(i - 14, i))) {
       return i;
     }
   }

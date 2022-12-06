36bf112e7b4ffe1154a6cd9477003f58a69ca797
Carl Vitullo <carl.vitullo@gmail.com>
2022-12-06 13:11:43 -0600

    Day 6 cleaned

--- 6/code.ts
+++ 6/code.ts
@@ -2,23 +2,17 @@ import { readFile } from "fs/promises";
 
 const input = (await readFile("./6/input")).toString("utf8");
 
-const hasMarker = (substr: string) => {
-  const x = new Set(substr.split(""));
-  if (x.size === 14) {
-    return true;
-  }
-};
-
-export const day6part1 = (input: string) => {
-  const out = [];
-  for (let i = 14; i < input.length; i++) {
-    if (hasMarker(input.slice(i - 14, i))) {
+const findMarker = (size: number, input: string) => {
+  for (let i = size; i < input.length; i++) {
+    if (new Set(input.slice(i - size, i).split("")).size === size) {
       return i;
     }
   }
 };
 
-export const day4part2 = (input: string) => {};
+export const day6part1 = (input: string) => findMarker(4, input);
+
+export const day6part2 = (input: string) => findMarker(14, input);
 
 console.log(day6part1(input));
-// console.log(day6part2(input));
+console.log(day6part2(input));
